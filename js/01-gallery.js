371fc1974978e29d11d9e914c64db0d1689b9067
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-08-11 18:23:27 +0300

    робив дз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const refs = {
+    galleryEl: document.querySelector(".gallery"),
+  };
+  const markupHtml = galleryItems
+    .map(({ description, original, preview }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join("");
 
-console.log(galleryItems);
+  refs.galleryEl.insertAdjacentHTML("afterbegin", markupHtml);
+  refs.galleryEl.addEventListener("click", openModal);
+
+  let instance;
+  
+  function openModal(e) {
+    e.preventDefault();
+    if (e.target.tagName !== "IMG") return;
+    const dataSource = e.target.dataset.source;
+  
+    instance = basicLightbox.create(`
+        <img src='${dataSource}'>
+    `);
+    instance.show();
+  }
+  
+  window.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+
+
