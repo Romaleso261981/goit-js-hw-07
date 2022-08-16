42d4abdab978d9c8b1561cc875725c54e501e8b6
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-08-16 09:06:50 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,24 +22,29 @@ refs.galleryEl.insertAdjacentHTML("afterbegin", markupHtml);
 refs.galleryEl.addEventListener("click", openModal);
 
 let instance;
-
 function openModal(e) {
   e.preventDefault();
   if (e.target.tagName !== "IMG") return;
   const dataSource = e.target.dataset.source;
 
-  instance = basicLightbox.create(`
+  instance = basicLightbox.create(
+    `
         <img src='${dataSource}'>
-    `);
+    `,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", closeModal);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
   instance.show();
-  window.addEventListener("keydown", addEventKay);
 }
 
-
-function addEventKay(e) {
-  console.log(e.code);
-  if (e.code === "Escape") {
-    instance.close();
-    window.removeEventListener('keydown', addEventKay);
-  }
+function closeModal(e) {
+  console.log(e.code );
+  if(e.code !== "Escape") return 
+  instance.close();
 }
